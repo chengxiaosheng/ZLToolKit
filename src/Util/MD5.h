427421b80ac25ad88e83e167c489ff79ba274f2e
kevin__cheng <kevin__cheng@outlook.com>
2026-07-24 00:15:30 +0800

    fix

--- src/Util/MD5.h
+++ src/Util/MD5.h
@@ -63,7 +63,6 @@ public:
     MD5& finalize();
     std::string hexdigest() const;
     std::string rawdigest() const;
-    friend std::ostream& operator<<(std::ostream&, MD5 md5);
 private:
     void init();
     typedef uint8_t uint1; //  8bit
